43461e3b82bf0c6cdd13d069832f68e4fcd07485
seigibus <seigibus@example.com>
2020-06-09 22:50:55 +0900

    #4 グループリストをクラスか

--- lib/screen/group/home.dart
+++ lib/screen/group/home.dart
@@ -10,7 +10,7 @@ class GroupHome extends StatelessWidget {
         title: Text(
           'FamilyMeal',
           style: TextStyle(
-            color: Colors.white
+              color: Colors.white
           ),
         ),
       ),
@@ -24,108 +24,9 @@ class GroupHome extends StatelessWidget {
             Text(
               'グループ一覧',
             ),
-          SizedBox(
-            height: 10,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-                  Card(
-                    margin: EdgeInsets.all(10.0),
-                    elevation: 5.0,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(4.0),
-                      ),
-                    ),
-                    child: Container(
-                      height: 100.0,
-                      padding: EdgeInsets.symmetric(horizontal: 10),
-                      child: Column(
-                        children: <Widget>[
-                          Row(
-                            children: <Widget>[
-                              Column(
-                                children: <Widget>[
-                                  SizedBox(
-                                    height: 10,
-                                  ),
-                                  Container(
-                                    height: 60.0,
-                                    width: 60.0,
-                                    decoration: new BoxDecoration(
-                                      shape: BoxShape.circle,
-                                      border: Border.all(
-                                        color: Colors.black12,
-                                      ),
-                                      image: new DecorationImage(
-                                          fit: BoxFit.fill,
-                                          image: new NetworkImage(
-                                              "https://so-sha.co.jp/cont/wp-content/themes/daishin-sosha/img/photo/menu/family/img02.png")),
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 5,
-                                  ),
-                                  Text(
-                                      "中村家",
-                                    style: TextStyle(
-                                      color: Colors.grey,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Column(
-                                children: <Widget>[
-                                  Row(
-
-                                    children: <Widget>[
-                                      SizedBox(
-                                        width: 10.0,
-                                      ),
-                                      FlatButton(
-                                          onPressed: () {
-                                            // メンバー一覧ページ
-                                          },
-                                          child: Text(
-                                            'メンバー3人',
-                                            style: TextStyle(
-                                              color: Colors.indigoAccent,
-                                            ),
-                                          ),
-                                      ),
-                                      SizedBox(
-                                        width: 10.0,
-                                      ),
-                                      FlatButton(
-                                          onPressed: () {
-                                            // フォロワー一覧ページ
-                                          },
-                                          child: Text(
-                                              'フォロワー10人',
-                                            style: TextStyle(
-                                              color: Colors.indigoAccent,
-                                            ),
-                                          ),
-                                      ),
-                                    ],
-                                  ),
-                                  Container(
-                                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                                    child: Text('美味しく楽しくがモットーです。'),
-                                  )
-                                ],
-                              ),
-                            ],
-                          )
-                        ],
-                      )
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+            belongGroupList(),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
@@ -136,3 +37,112 @@ class GroupHome extends StatelessWidget {
     );
   }
 }
+
+class belongGroupList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        SizedBox(
+          height: 10.0,
+        ),
+        Card(
+          margin: EdgeInsets.all(10.0),
+          elevation: 5.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(4.0),
+            ),
+          ),
+          child: Container(
+              height: 100.0,
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            height: 60.0,
+                            width: 60.0,
+                            decoration: new BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                color: Colors.black12,
+                              ),
+                              image: new DecorationImage(
+                                  fit: BoxFit.fill,
+                                  image: new NetworkImage(
+                                      "https://so-sha.co.jp/cont/wp-content/themes/daishin-sosha/img/photo/menu/family/img02.png")),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            "中村家",
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Row(
+
+                            children: <Widget>[
+                              SizedBox(
+                                width: 10.0,
+                              ),
+                              FlatButton(
+                                onPressed: () {
+                                  // メンバー一覧ページ
+                                },
+                                child: Text(
+                                  'メンバー3人',
+                                  style: TextStyle(
+                                    color: Colors.indigoAccent,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 10.0,
+                              ),
+                              FlatButton(
+                                onPressed: () {
+                                  // フォロワー一覧ページ
+                                },
+                                child: Text(
+                                  'フォロワー10人',
+                                  style: TextStyle(
+                                    color: Colors.indigoAccent,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Container(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 5),
+                            child: Text('美味しく楽しくがモットーです。'),
+                          )
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              )
+          ),
+        ),
+      ],
+    )
+    ,;
+  }
+}
+
