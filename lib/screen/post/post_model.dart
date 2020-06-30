b09346f956925a24eb5f0b2ab790c15ff6485937
seigibus <seigibus@example.com>
2020-06-30 20:29:47 +0900

    クライアントから検索

--- lib/screen/post/post_model.dart
+++ lib/screen/post/post_model.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 
+import 'package:algolia/algolia.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:weight/models/Group.dart';
 import 'package:weight/models/Post.dart';
 
@@ -16,7 +18,8 @@ class PostModel extends ChangeNotifier{
   String profileURL = "";
 
   File currentImage;
-
+  bool searching = false;
+  bool isLike = false;
   List<Post> posts = [];
 
   Future fetchPost(Group group)async {
@@ -27,16 +30,19 @@ class PostModel extends ChangeNotifier{
     List<Future<Post>> tasks = docIds.map((id) async {
       return _fetchMyPost(id);
     }).toList();
-
     final posts = await Future.wait(tasks);
     this.posts = posts;
-
     notifyListeners();
   }
 
-  Future _fetchMyPost(String id) async {
+  Future<Post> _fetchMyPost(String id) async {
+    final user = await auth.currentUser();
     final doc = await db.collection('posts').document(id).get();
-    final posts = Post(name:doc['title'],text:doc['text'],postID:id, groupID:doc['groupID'], created:doc['created'],imageURL:doc['imageURL'],likes:doc['like']);
+    final likePost = await db.collection('users').document(user.uid).collection('likePost').getDocuments();
+    final ids = likePost.documents.map((doc) => doc.documentID);
+    final bool isLike = ids.contains(id);
+    final posts = Post(name:doc['title'],text:doc['text'],postID:id, groupID:doc['groupID'], created:doc['created'],imageURL:doc['imageURL'],likes:doc['like'],isLike:isLike);
+    this.isLike = isLike;
     return posts;
   }
 
@@ -52,6 +58,51 @@ class PostModel extends ChangeNotifier{
     final metaData = StorageMetadata(contentType: "image/png");
     StorageUploadTask task = ref.putFile(image, metaData);
     profileURL = await (await task.onComplete).ref.getDownloadURL();
+    notifyListeners();
+  }
+  // algoliaに検索をかける
+  Future searchPost(text) async {
+    Algolia algolia = Algolia.init(
+      applicationId: DotEnv().env["ALGOLIA_APP_KEY"],
+      apiKey: DotEnv().env["SEARCH_API_KEY"],
+    );
+    AlgoliaQuery query = algolia.instance.index('posts');
+    query = query.search(text);
+
+    final results = (await query.getObjects()).hits;
+    final postIds = results.map((result) => result.objectID);
+    List<Future<Post>> tasks = postIds.map((id) async {
+      return _fetchMyPost(id);
+    }).toList();
+    final posts = await Future.wait(tasks);
+    this.posts = posts;
+    print(posts.toString());
+    this.searching = false;
+    notifyListeners();
+  }
+
+  // いいねの処理
+  Future likePost(Post post) async {
+    final user = await auth.currentUser();
+    db.collection('posts').document(post.postID).collection('likeUsers').document(user.uid).setData({
+      'user': user.uid,
+    });
+    db.collection('posts').document(post.postID).updateData({
+      'like': FieldValue.increment(1),
+    });
+    db.collection('users').document(user.uid).collection('likePost').document(post.postID).setData({
+      'postID': post.postID,
+      'group' : post.groupID,
+    });
+    this.isLike = true;
+    notifyListeners();
+  }
+  // いいね解除したときの処理
+  Future unLikePost(Post post) async {
+    final user =await auth.currentUser();
+    db.collection('posts').document(post.postID).collection('likeUsers').document(user.uid).delete();
+    db.collection('users').document(user.uid).collection('likePost').document(post.postID).delete();
+    this.isLike = false;
     notifyListeners();
   }
 }
---
+++ lib/screen/post/post_search.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:weight/models/Post.dart';
+import 'package:weight/models/user.dart';
+import 'package:weight/screen/User/user_model.dart';
+import 'package:weight/screen/home/home.dart';
+import 'package:weight/screen/home/home_model.dart';
+import 'package:weight/screen/post/post_model.dart';
+
+class PostSearch extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<PostModel>(
+      create: (_) => PostModel(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('投稿検索'),
+        ),
+        body: Consumer<PostModel>(builder: (context, model, child) {
+          TextEditingController _searchText = TextEditingController(text: "");
+          final posts = model.posts;
+          return ListView(
+            children: <Widget>[
+              TextField(
+                controller: _searchText,
+                decoration: InputDecoration(hintText: "Search query here..."),
+              ),
+              FlatButton(
+                color: Colors.blue,
+                child: Text(
+                  "Search",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  model.searching = true;
+                  model.searchPost(_searchText.text);
+                },
+              ),
+              Expanded(
+                child: model.searching == true
+                    ? Center(
+                  child: Text("Searching, please wait..."),
+                )
+                    : model.posts.length == 0
+                    ? Center(
+                  child: Text("該当する投稿が見つかりませんでした"),
+                )
+                    :PostList(posts:model.posts),
+              ),
+            ],
+          );
+        }),
+      ),
+    );
+  }
+}
+class PostList extends StatelessWidget {
+  PostList({this.posts});
+  final List<Post> posts;
+  @override
+  Widget build(BuildContext context) {
+    final postList = posts.map((post) => Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Padding(
+          padding:
+          const EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  // iconImage(group)
+                  Container(
+                    height: 40.0,
+                    width: 40.0,
+                    decoration: new BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: new DecorationImage(
+                          fit: BoxFit.fill,
+                          image: new NetworkImage(
+                              "https://so-sha.co.jp/cont/wp-content/themes/daishin-sosha/img/photo/menu/family/img02.png")),
+                    ),
+                  ),
+                  new SizedBox(
+                    width: 10.0,
+                  ),
+                  // groupname
+                  new Text(
+                    "中村家",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              new IconButton(
+                icon: Icon(Icons.more_vert),
+                onPressed: null,
+              )
+            ],
+          ),
+        ),
+        // postImage
+        Flexible(
+          fit: FlexFit.loose,
+          child: Image.network(
+            post.imageURL,
+            fit: BoxFit.cover,
+          ),
+        ),
+        postActions(post: post),
+        Padding(
+          padding:
+          EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              new Text(
+                post.name,
+              ),
+              SizedBox(
+                width: 10.0,
+              ),
+              Text(post.text)
+            ],
+          ),
+        )
+      ],
+    ), ).toList();
+    return ListView(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      children: postList,
+    );
+  }
+}
+
+class postActions extends StatelessWidget {
+  postActions({this.post});
+  final Post post;
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<PostModel>(
+      create: (_) =>PostModel(),
+      child: Consumer<PostModel>(
+          builder: (context, model, child) {
+            return Padding(
+              padding: EdgeInsets.all(0.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  (model.isLike)
+                      ? IconButton(
+                    icon: Icon(Icons.star),
+                    onPressed: () async {
+                      post.isLike = false;
+                      await model.unLikePost(post);
+                    },
+                  )
+                      : IconButton(
+                    icon: Icon(Icons.star_border),
+                    onPressed: () async {
+                      post.isLike = true;
+                      await model.likePost(post);
+// いいねの処
+                    },
+                  ),
+                  Text('300'),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.share),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            );
+          }
+      ),
+    );
+  }
+}
+
+
+
+
