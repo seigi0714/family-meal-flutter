b48380690e63f473b6b1d8dacfb382f1f594e6f7
seigibus <seigibus@example.com>
2020-06-30 20:27:53 +0900

    firestoreをトリガーにalgoliaにデータをセット

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,86 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+const functions = require('firebase-functions');
+const algoliasearch = require('algoliasearch');
+const ALGOLIA_ID = functions.config().algolia.app_id;
+const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key;
+const ALGOLIA_SEARCH_KEY = functions.config().algolia.search_key;
+const ALGOLIA_INDEX_NAME = 'posts';
+
+const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
+
+
+
+
+exports.createIndex = functions.database.ref('posts/{postID}').onCreate((snap, context) => {
+ const data = snap.data();
+    // AlgoliaのIndexに保存する情報
+  const algoliaObject = {
+    groupID: data.groupID,
+    title: data.title,
+    text: data.text,
+  };
+  algoliaObject.objectID = context.params.postId;
+
+  // Indexを保存
+  const index = client.initIndex(ALGOLIA_INDEX_NAME);
+  return index.saveObject(algoliaObject);
+});
+exports.removeIndex = functions.database.ref('posts/{postID}').onDelete((snap, context)  => {
+   const objectID = context.params.postId;
+   index.deleteObject(objectID)
+});
+exports.editIndex  = functions.database.ref('posts/{postID}').onUpdate((snap, context) => {
+ const afterData = snap.after.data();
+ const algoliaObject = {
+     groupID: afterData.groupID,
+     title: afterData.title,
+     text: afterData.text,
+   };
+   algoliaObject.objectID = context.params.postId;
+});
+
+const admin = require('firebase-admin');
+const { firebaseConfig } = require('firebase-functions');
+admin.initializeApp();
+
+const db = admin.firestore();
+const userRef = db.collection('users');
+const groupRef = db.collection('groups');
+const postRef = db.collection('posts');
+exports.addGroup = functions.https.onCall((data, context) =>{
+      groupRef.add({
+          name: data.name,
+          text: data.text,
+          iconImage: data.image,
+          GroupUser: context.auth.uid,
+          UserCount: 1,
+          Follower: 0,
+      }).then(function(docRef) {
+        return userRef.doc(context.auth.uid).collection('belongingGroup').doc(docRef.id).set({
+           joinAt: admin.firestore.FieldValue.serverTimestamp(),
+           GroupID: docRef.id
+       })
+      })
+        .catch(function(error) {
+           throw console.error("Error adding document: ", error);
+        });
+})
+
+exports.addPost = functions.https.onCall((data, context) =>{
+    postRef.add({
+      title: data.title,
+      text: data.text,
+      imageURL: data.image,
+      GroupID: data.groupID,
+      like: 0,
+      created: admin.firestore.FieldValue.serverTimestamp(),
+    }).then(function(docRef){
+      return groupRef.doc(data.groupID).collection('posts').doc(docRef.id).set({
+        postID: docRef.id
+      })
+    }).catch(function(error){
+      throw console.error("Error adding document: ", error);
+    });
+})
