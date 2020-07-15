5b2f8ef67d4b8748998c5fe61ee3a197195785a2
seigibus <seigibus@example.com>
2020-07-15 21:31:22 +0900

    コメントのモデル作成

---
+++ lib/models/Comment.dart
@@ -0,0 +1,8 @@
+class Comment {
+  Comment({this.postID,this.userID,this.text,this.created,this.isGroupUser});
+  final String postID;
+  final String userID;
+  final String text;
+  final String created;
+  final bool isGroupUser;
+}
