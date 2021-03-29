a2c157f979fd61af657df542a8cdefb5cc1dacc6
seigibus <seigibus@icloud.com>
2021-03-29 20:26:32 +0900

    test

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,19 +1,1 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 
-class User {
-  String userID;
-  String name;
-  String iconURL;
-  bool isBelong;
-  bool isInv;
-
-  User({this.userID, this.name,this.iconURL,this.isBelong,this.isInv});
-}
-
-class UserData {
-  final String userID;
-  final String name;
-  final Timestamp createAt;
-
-  UserData({ this.userID, this.name, this.createAt});
-}
